3919e3c53d88c349aa63abbf9a762f9fc800fe97
Manikanta Kotthapalli <k.manikanta33@hotmail.com>
2017-07-20 18:04:02 -0700

    added weblayer

--- GeoNEWS/GeoNEWS/GeoNEWS.Shared/MapPage.xaml.cs
+++ GeoNEWS/GeoNEWS/GeoNEWS.Shared/MapPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Location;
 using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Portal;
+using Esri.ArcGISRuntime.Security;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.UI;
 using Esri.ArcGISRuntime.Xamarin.Forms;
@@ -12,118 +15,144 @@ namespace GeoNEWS
 {
 	public partial class MapPage : ContentPage
     {
-        private Dictionary<string, Uri> _iconUris;
-        private Dictionary<string, Uri> _buttonUris;
+        // Constants for OAuth-related values ...
+        // URL of the server to authenticate with
+        private const string ServerUrl = "https://summerinterns.maps.arcgis.com/sharing/rest";
+        // TODO: Provide the client ID for your app (registered with the server)
+        private const string ClientId = "o4mlpsRN6IJARJSK";
+        // TODO: [optional] Provide the client secret for the app (only needed for the OAuthAuthorizationCode auth type)
+        private const string ClientSecret = "43b4d51db14d4100b7d14d4d917d4a3e";
+        // TODO: Provide a URL registered for the app for redirecting after a successful authorization
+        private const string RedirectUrl = "https://developers.arcgis.com";
+        // TODO: Provide an ID for a secured web map item hosted on the server
+        private const string WebMapId = "6291995bb09f4f989148b6f58e491ff3";
 
         public MapPage()
         {
             InitializeComponent();
 
-            Title = "Show Callout";
-            InitMap();
-
-            FillComboBoxes();
+            Title = "The Mapsetters";
+            Initialize();
         }
 
-
-        private void InitMap()
+        private void Initialize()
         {
-            var map = new Esri.ArcGISRuntime.Mapping.Map(Basemap.CreateLightGrayCanvas());
-            var uriCity = new Uri("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/0");
-            var uriHwy = new Uri("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/1");
-            var uriCounty = new Uri("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/3");
+            LoadUIElements();
+            // Set up the AuthenticationManager to use OAuth for secure ArcGIS Online requests
+            UpdateAuthenticationManager();
 
-            var cityLayer = new FeatureLayer(uriCity);
-            cityLayer.Id = "city";
-            var hwyLayer = new FeatureLayer(uriHwy);
-            hwyLayer.Id = "hwy";
-            var countyLayer = new FeatureLayer(uriCounty);
-            countyLayer.Id = "county";
+            // Display a secured web map from ArcGIS Online (will be challenged to log in)
+            DisplayWebMap();
 
-            map.OperationalLayers.Add(countyLayer);
-            map.OperationalLayers.Add(hwyLayer);
-            map.OperationalLayers.Add(cityLayer);
-
-            MyMapView.Map = map;
-            MyMapView.GeoViewTapped += MyMapView_GeoViewTapped;
-
-            MyMapView.SetViewpointAsync(new Viewpoint(34.056077, -117.198855, 15000));
+            MyMapView.LocationDisplay.IsEnabled = true;
+            //MyMapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Navigation;
         }
 
-        private void FillComboBoxes()
+        private void LoadUIElements()
         {
-            _iconUris = new Dictionary<string, Uri>();
-            _iconUris.Add("Maple Leaf", new Uri("http://static.arcgis.com/images/Symbols/Business/esriBusinessMarker_236.png"));
-            _iconUris.Add("Earth", new Uri("http://static.arcgis.com/images/Symbols/PeoplePlaces/Globe.png"));
-            _iconUris.Add("Car", new Uri("http://static.arcgis.com/images/Symbols/Transportation/CarYellowFront.png"));
-            _iconUris.Add("None", null);
-
-            //foreach (var icon in _iconUris.Keys)
-            //{
-            //    CalloutIconPicker.Items.Add(icon);
-            //}
-
-            //CalloutIconPicker.SelectedIndex = 0;
-
-            _buttonUris = new Dictionary<string, Uri>();
-            _buttonUris.Add("Star", new Uri("http://static.arcgis.com/images/Symbols/Shapes/BlueStarLargeB.png"));
-            _buttonUris.Add("Question", new Uri("http://static.arcgis.com/images/Symbols/PeoplePlaces/InformationQuestion.png"));
-            _buttonUris.Add("Exclaim", new Uri("http://static.arcgis.com/images/Symbols/Transportation/WarningRed.png"));
-            _buttonUris.Add("None", null);
-
-            //foreach (var btn in _buttonUris.Keys)
-            //{
-            //    CalloutButtonPicker.Items.Add(btn);
-            //}
-
-            //CalloutButtonPicker.SelectedIndex = 0;
+            imgLocation.Source = "user.jpg";
+            imgLocation.GestureRecognizers.Add(new TapGestureRecognizer(OnlocationimgTap));
+            imglistview.Source = "earth.jpg";
+            imgprofile.Source = "user.jpg";
         }
 
-        private async void MyMapView_GeoViewTapped(object sender, GeoViewInputEventArgs e)
+        private void OnlocationimgTap(View arg1, object arg2)
         {
-            //if (!UseCustomToggleButton.IsToggled) // Identify Features
-            //{
-            //    var iconName = CalloutIconPicker.Items[CalloutIconPicker.SelectedIndex];
-            //    var buttonName = CalloutButtonPicker.Items[CalloutButtonPicker.SelectedIndex];
-            //    var iconImageUrl = _iconUris[iconName]; // CalloutIconPicker.SelectedValue as Uri;
-            //    var buttonImageUrl = _buttonUris[buttonName]; // CalloutButtonPicker.SelectedValue as Uri;
-
-            //    var feature = await FindFeatureNearClickAsync(e.Position);
-            //    CalloutDefinition calloutDef = null;
-
-            //    if (feature != null)
-            //    {
-            //        calloutDef = new CalloutDefinition(feature);
-            //        if (iconImageUrl != null)
-            //        {
-            //            var mapleMarker = new PictureMarkerSymbol(iconImageUrl);
-            //            await calloutDef.SetIconFromSymbolAsync(mapleMarker);
-            //        }
-
-            //        if (buttonImageUrl != null)
-            //        {
-            //            var buttonImg = new RuntimeImage(buttonImageUrl);
-            //            calloutDef.ButtonImage = buttonImg;
-            //            calloutDef.OnButtonClick += OnCalloutButtonClick;
-            //        }
-
-            //        calloutDef.Tag = feature;
-            //        if (feature.Attributes.ContainsKey("pop2000"))
-            //        {
-            //            calloutDef.DetailText = "Here's another value: " + feature.Attributes["pop2000"];
-            //        }
-
-            //        MyMapView.ShowCalloutForGeoElement(feature, e.Position, calloutDef);
-            //    }
-            //    else
-            //    {
-            //        calloutDef = new CalloutDefinition("You missed ...", "no features here");
-            //        var skullMarkerSym = new PictureMarkerSymbol(new Uri("http://static.arcgis.com/images/Symbols/Transportation/SkullandCrossbones.png"));
-            //        await calloutDef.SetIconFromSymbolAsync(skullMarkerSym);
-            //        MyMapView.ShowCalloutAt(e.Location, calloutDef);
-            //    }
+            MyMapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Navigation;
         }
-           
-    
+
+        private void UpdateAuthenticationManager()
+        {
+            // Register the server information with the AuthenticationManager
+            Esri.ArcGISRuntime.Security.ServerInfo serverInfo = new ServerInfo
+            {
+                ServerUri = new Uri(ServerUrl),
+                OAuthClientInfo = new OAuthClientInfo
+                {
+                    ClientId = ClientId,
+                    RedirectUri = new Uri(RedirectUrl)
+                }
+            };
+
+            // If a client secret has been configured, set the authentication type to OAuthAuthorizationCode
+            if (!string.IsNullOrEmpty(ClientSecret))
+            {
+                // Use OAuthAuthorizationCode if you need a refresh token (and have specified a valid client secret)
+                serverInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthAuthorizationCode;
+                serverInfo.OAuthClientInfo.ClientSecret = ClientSecret;
+            }
+            else
+            {
+                // Otherwise, use OAuthImplicit
+                serverInfo.TokenAuthenticationType = TokenAuthenticationType.OAuthImplicit;
+            }
+
+            // Register this server with AuthenticationManager
+            AuthenticationManager.Current.RegisterServer(serverInfo);
+
+            // Use a function in this class to challenge for credentials
+            AuthenticationManager.Current.ChallengeHandler = new ChallengeHandler(CreateCredentialAsync);
+
+            // Note: In a WPF app, you need to associate a custom IOAuthAuthorizeHandler component with the AuthenticationManager to 
+            //     handle showing OAuth login controls (AuthenticationManager.Current.OAuthAuthorizeHandler = new MyOAuthAuthorize();).
+            //     The UWP AuthenticationManager, however, uses a built-in IOAuthAuthorizeHandler (based on WebAuthenticationBroker).
+        }
+
+        private async void DisplayWebMap()
+        {
+            // Display a web map hosted in a portal. If the web map item is secured, AuthenticationManager will
+            // challenge for credentials
+            try
+            {
+                // Connect to a portal (ArcGIS Online, for example)
+                ArcGISPortal arcgisPortal = await ArcGISPortal.CreateAsync(new Uri(ServerUrl));
+
+                // Get a web map portal item using its ID
+                // If the item is secured (not shared publicly) the user will be challenged for credentials at this point
+                PortalItem portalItem = await PortalItem.CreateAsync(arcgisPortal, WebMapId);
+
+                // Create a new map with the portal item
+                Map myMap = new Map(portalItem);
+
+                // Assign the map to the MapView.Map property to display it in the app
+                MyMapView.Map = myMap;
+                await myMap.RetryLoadAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public async Task<Credential> CreateCredentialAsync(CredentialRequestInfo info)
+        {
+            // ChallengeHandler function for AuthenticationManager that will be called whenever access to a secured
+            // resource is attempted
+            OAuthTokenCredential credential = null;
+
+            try
+            {
+                // Create generate token options if necessary
+                if (info.GenerateTokenOptions == null)
+                {
+                    info.GenerateTokenOptions = new GenerateTokenOptions();
+                }
+
+                // AuthenticationManager will handle challenging the user for credentials
+                credential = await AuthenticationManager.Current.GenerateCredentialAsync
+                    (
+                            info.ServiceUri,
+                            info.GenerateTokenOptions
+                    ) as OAuthTokenCredential;
+            }
+            catch (Exception ex)
+            {
+                // Exception will be reported in calling function
+                throw (ex);
+            }
+
+            return credential;
+        }
+
     }
 }
